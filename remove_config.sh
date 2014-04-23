868bd976dd7ff1639b9fe7520e5808fa5b308491
Gokul Kartha <gokul.kartha@ixonos.com>
2014-04-23 12:25:19 +0300

    updated path

---
+++ remove_config.sh
@@ -0,0 +1,12 @@
+sudo rm -rf aclocal.m4 autom4te.cache build-aux config.h* config.log \
+config.status configure libixonos-ml-common-0.1.la libtool Makefile \
+Makefile.in stamp-h1 testinstall .deps .libs \
+common/src/.deps common/src/.dirstamp common/src/*.lo \
+vnc/src/.deps vnc/src/.dirstamp vnc/src/*.lo \
+upnp/src/.deps upnp/src/.dirstamp upnp/src/*.lo \
+cdb/src/.deps cdb/src/.dirstamp cdb/src/*.lo \
+rtp/src/.deps rtp/src/.dirstamp rtp/src/*.lo \
+dap/src/.deps dap/src/.dirstamp dap/src/*.lo \
+ivi-connect-client-linux-mirrorlink-1.0.pc \
+ivi-connect-client-linux-mirrorlink-1.0.tar.gz \
+libixonos-ml-*.la
